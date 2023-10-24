0eeb0671d6a5e4698799343553a422ae2dccf5ae
Satyam Soni <107421155+SatyamSoni676@users.noreply.github.com>
2023-10-24 22:33:05 +0530

    13

--- Arrays/Permutation in String.py
+++ Arrays/Permutation in String.py
@@ -9,6 +9,6 @@ def checkInclusion(self, s1: str, s2: str) -> bool:
 		if i >= w and s2[i-w] in cntr: 
 			cntr[s2[i-w]] += 1
 
-		if all([cntr[i] == 0 for i in cntr]): # see optimized code below
+		if all([cntr[i] == 0 for i in cntr]): 
 			return True
 	return False
