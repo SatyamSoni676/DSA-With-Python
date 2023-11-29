d6e6f0a848f417c5893fcfcdd7204fbf3e84adfa
Satyam <satyamsoni676@gmail.com>
2023-11-29 09:08:53 +0530

    Create GenerateParentheses.py

---
+++ Recursion and Backtracking/GenerateParentheses.py
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/generate-parentheses/
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(s='', left=0, right=0):
+            if len(s) == 2*n:
+                res.append(s)
+                return
+            if left < n:
+                backtrack(s+'(', left+1, right)
+            if left > right:
+                backtrack(s+')', left, right+1)
+        
+        res = []
+        backtrack()
+        return res
