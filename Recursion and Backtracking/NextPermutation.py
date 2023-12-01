9e20fa83a618e20ee830f67850d11306acd61377
Satyam <satyamsoni676@gmail.com>
2023-12-01 11:07:09 +0530

    Create NextPermutation.py

---
+++ Recursion and Backtracking/NextPermutation.py
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/next-permutation/
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        b1 = -1
+
+        for i in range(len(nums)-1):
+            if nums[i] < nums[i+1]:
+                b1 = i
+
+        if b1 == -1:
+            nums.reverse()
+            return nums
+
+        for j in range(len(nums) - 1, b1, -1):
+            if nums[j] > nums[b1]:
+                nums[j], nums[b1] = nums[b1], nums[j]
+                break
+
+        nums[b1+1:] = reversed(nums[b1+1:])
